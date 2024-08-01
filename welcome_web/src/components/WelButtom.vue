395f59f8cda603dbf8da1a3a770b64871911a005
xinxin <3497906382@qq.com>
2024-08-01 21:58:10 +0800

    8.1 lyx

--- welcome_web/src/components/WelButtom.vue
+++ welcome_web/src/components/WelButtom.vue
@@ -26,7 +26,6 @@
 }
 
 .buttom {
-  /* position: relative; */
   display: flex;
   height: 180px;
   width: 100%;
@@ -45,7 +44,7 @@
   flex: 1;
   padding-left: 5%;
   padding-top: 3%;
-  /* width: 8%; */
+  width: 8%;
 }
 .words {
   flex: 4;
@@ -56,4 +55,36 @@
   flex: 2;
   padding-top: 3%;
 }
+
+/* 响应式 */
+@media (max-width: 1024px) {  
+  .buttom {
+    font-size: 13px;
+  }
+
+  .img1 {
+    height: 80px;
+    width: 80px;
+  }
+
+  .img2 {
+  width: 150px;
+  }
+}  
+  
+@media (max-width: 768px) {  
+  .buttom {
+    font-size: 10px;
+    height: 120px;
+  }
+
+  .img1 {
+    height: 50px;
+    width: 50px;
+  }
+
+  .img2 {
+    width: 100px;
+  }
+}  
 </style>
