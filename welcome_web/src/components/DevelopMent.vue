092179d1f04317bab3033bb219ae1b6a9ae22fea
xinxin <3497906382@qq.com>
2024-08-04 13:12:52 +0800

    index 8.4 lyx

---
+++ welcome_web/src/components/DevelopMent.vue
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+<template>
+  <div class="main">
+      <div class="bigBack">
+        
+      </div>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  box-sizing: border-box; 
+  padding-top: 60px; 
+}
+.bigBack {
+  height: 384px;
+  width: 100%;
+  background-size: cover;
+  background-image: url(@/assets/banner.jpg);
+  background-position: center;
+  color: #fff;
+}
+</style>
