ff6818875b9d4b9e9c294fd5bf0c577d4cb8815b
Shu Yi Kang <185901261@qq.com>
2024-08-17 10:51:51 +0800

    chore(all): [build] 性能优化

--- welcome_web/src/router/index.js
+++ welcome_web/src/router/index.js
@@ -1,16 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router"
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    { path: '/', redirect: '/index'},
-    { path: '/index', component: () => import('@/views/index/indexPage.vue')},
-    { path: '/about',
-      component: () => import('@/views/about/aboutPage.vue')
-    },
-    { path: '/employment', component: () => import('@/views/employment/employmentPage.vue') },
-    { path: '/join', component: () => import('@/views/join/joinPage.vue') }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        { path: "/", redirect: "/index" },
+        { path: "/index", component: () => import("@/views/index/indexPage.vue") },
+        { path: "/about", component: () => import("@/views/about/aboutPage.vue") },
+        { path: "/employment", component: () => import("@/views/employment/employmentPage.vue") },
+        { path: "/join", component: () => import("@/views/join/joinPage.vue") },
+    ],
 })
 
 export default router
--- welcome_web/vite.config.js
+++ welcome_web/vite.config.js
@@ -1,36 +1,45 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import AutoImport from "unplugin-auto-import/vite"
+import Components from "unplugin-vue-components/vite"
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
+import vitePluginImageMini from "vite-plugin-imagemin"
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server:{
-    open:true,  // 在启动开发服务器时，会自动打开浏览器并访问指定的地址
-    proxy:{
-      '/api':{
-        target:'https://abmy.online/welcome', // 接口域名
-        changeOrigin:true, // 是否跨域
-        ws:true,  // 是否代理 websockets
-        secure:true,  // 是否htpps接口
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+    plugins: [
+        vue(),
+        vitePluginImageMini({
+            optipng: {
+                optimizationLevel: 7,
+            },
+            pngquant: {
+                quality: [0.8, 0.9],
+            },
+        }),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+    server: {
+        open: true, // 在启动开发，会自服务器时动打开浏览器并访问指定的地址
+        proxy: {
+            "/api": {
+                target: "https://abmy.online/welcome", // 接口域名
+                changeOrigin: true, // 是否跨域
+                ws: true, // 是否代理 websockets
+                secure: true, // 是否htpps接口
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
+    },
 })
